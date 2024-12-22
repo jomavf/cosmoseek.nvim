4487f737a9777bd220177ba949c8d2a6a83e6932
Jose Veliz <jose.veliz.francia@gmail.com>
2024-12-22 12:44:17 -0500

    Add `CosmoSeek` user command and file type auto command
    - Introduced a `CosmoSeek` user command for invoking `search_api` with arguments.
    - Added a `FileType` autocmd for `qf` buffers to detect `CosmoSeek` results.
    - Mapped `<CR>` in `qf` buffers to open selected quickfix items when the title matches `CosmoSeek`.
    - Enhanced integration between search results and quickfix list interaction.

--- lua/cosmoseek/init.lua
+++ lua/cosmoseek/init.lua
@@ -4,6 +4,25 @@ local search = require("cosmoseek.search")
 function M.setup(opts)
 	opts = opts or {}
 	M.options = {}
+
+	vim.api.nvim_create_user_command("CosmoSeek", function(cmd_opts)
+		search.search_api(cmd_opts.args)
+	end, { nargs = 1, desc = "Busca en la API de grep.app y muestra resultados en la quickfix (o loclist)" })
+
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "qf",
+		callback = function()
+			local qfinfo = vim.fn.getqflist({ title = true })
+			if qfinfo.title == "CosmoSeek" then
+				vim.keymap.set(
+					"n",
+					"<CR>",
+					require("cosmoseek.search").on_search_open,
+					{ buffer = true, desc = "Ejecuta open_selected_qf" }
+				)
+			end
+		end,
+	})
 end
 
 function M.search(query)
